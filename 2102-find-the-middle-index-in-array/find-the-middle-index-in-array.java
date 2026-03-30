3c244e1f94ac081a0ed5de693caa2ef41f1d61da
Sagar kandpal <149861518+sagarkandpal@users.noreply.github.com>
2026-03-30 14:10:29 +0530

    Time: 0 ms (100.00%) | Memory: 43.5 MB (68.32%) - LeetSync

--- 2102-find-the-middle-index-in-array/find-the-middle-index-in-array.java
+++ 2102-find-the-middle-index-in-array/find-the-middle-index-in-array.java
@@ -1,23 +1,50 @@
 class Solution {
     public int findMiddleIndex(int[] nums) {
-        
-        int leftsum = 0;
-        int rightsum = 0;
-        int n = nums.length;
 
+        int sum = 0;
+        int n = nums.length;
         for(int i=0; i<n; i++){
-            leftsum = 0;
-            rightsum = 0;
-            for(int j=0; j<i; j++){
-                leftsum += nums[j];
-            }
-            for(int j=i+1; j<n; j++){
-                rightsum += nums[j];            
-            }
+            sum = sum + nums[i];
+        }
+
+        int leftsum = 0;
+        for(int i=0; i<n; i++){
+            int rightsum = sum - nums[i] - leftsum;
             if(leftsum == rightsum){
                 return i;
             }
+            leftsum = leftsum + nums[i];
         }
+
         return -1;
+
+        
+
+
+
+
+
+
+
+
     }
 }
+
+// int leftsum = 0;
+//         int rightsum = 0;
+//         int n = nums.length;
+
+//         for(int i=0; i<n; i++){
+//             leftsum = 0;
+//             rightsum = 0;
+//             for(int j=0; j<i; j++){
+//                 leftsum += nums[j];
+//             }
+//             for(int j=i+1; j<n; j++){
+//                 rightsum += nums[j];            
+//             }
+//             if(leftsum == rightsum){
+//                 return i;
+//             }
+//         }
+//         return -1;
