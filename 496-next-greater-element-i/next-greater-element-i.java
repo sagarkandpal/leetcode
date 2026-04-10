a8b09a5c21dd858b747f111bc31bdec51d93638f
Sagar kandpal <149861518+sagarkandpal@users.noreply.github.com>
2026-04-10 18:19:53 +0530

    Time: 4 ms (74.47%) | Memory: 45.2 MB (52.93%) - LeetSync

--- 496-next-greater-element-i/next-greater-element-i.java
+++ 496-next-greater-element-i/next-greater-element-i.java
@@ -1,29 +1,56 @@
 class Solution {
     public int[] nextGreaterElement(int[] nums1, int[] nums2) {
         
-        int n = nums1.length;
-        int m = nums2.length;
+        int[] result = new int[nums1.length];
+        HashMap<Integer, Integer> map = new HashMap<>();
+        Stack<Integer> st = new Stack<>();
 
-        int[] result = new int[n];
-        for(int i=0; i<n; i++){
-            int p = nums1[i];
-            int idx = -1;
-            
-            int j;
-            for(j=0; j<m; j++){
-                if(nums1[i] ==  nums2[j]){
-                    break;
-                }
+        for(int i=0; i<nums2.length; i++){
+            while(!st.isEmpty() && st.peek() < nums2[i]){
+                map.put(st.pop(), nums2[i]);
             }
+            st.push(nums2[i]);
+        }
 
-            for(int k=j+1; k<m; k++){
-                if(nums2[k] > p){
-                    idx = nums2[k];
-                    break;
-                }
-            }
-            result[i] = idx;
-        }      
+        for(int i : st){
+            map.put(i, -1);
+        }
+
+        for(int i=0; i<nums1.length; i++){
+            result[i] = map.get(nums1[i]);
+        }
+
         return result;
     }
 }
+
+
+
+
+
+
+// brute force
+
+//  int n = nums1.length;
+//         int m = nums2.length;
+
+//         int[] result = new int[n];
+//         for(int i=0; i<n; i++){
+//             int p = nums1[i];
+//             int idx = -1;
+            
+//             int j;
+//                 if(nums1[i] ==  nums2[j]){
+//                     break;
+//                 }
+//             }
+
+//             for(int k=j+1; k<m; k++){
+//                 if(nums2[k] > p){
+//                     idx = nums2[k];
+//                     break;
+//                 }
+//             }
+//             result[i] = idx;
+//         }      
+//         return result;
